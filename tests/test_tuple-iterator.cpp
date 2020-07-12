0f84ce8158b37a4146633357d45c19712113533f
AndreyNiemand <lafortaviro@gmail.com>
2020-07-12 21:44:27 +0300

    Added basic functionality to the tuple-iterator. Typed tests.

--- tests/test_tuple-iterator.cpp
+++ tests/test_tuple-iterator.cpp
@@ -1,15 +1,68 @@
 
 #include "module.hpp"
-
-#include <tuple>
 #include "tuple-iterator.hpp"
 
 BOOST_AUTO_TEST_SUITE(test_tuple_iterator);
 
-BOOST_AUTO_TEST_CASE(_begin)
+BOOST_AUTO_TEST_CASE(test_begin_and_end)
 {
-    std::tuple t = {1,2,3};
-    auto it = QoUtils::begin(t);
+    using namespace QoUtils;
+
+    std::tuple t = {1, 2.2, "3", true, 7U};
+    auto it = begin(t);
+    {
+        auto v = (int*)it;
+        BOOST_REQUIRE_NE(v, nullptr);
+        BOOST_CHECK_EQUAL(*v, std::get<0>(t));
+    }
+    ++it;
+    {
+        auto v = (double*)it;
+        BOOST_REQUIRE_NE(v, nullptr);
+        BOOST_CHECK_EQUAL(*v, std::get<1>(t));
+    }
+    it++;
+    {
+        auto v = (char*)it;
+        BOOST_REQUIRE_EQUAL(v, nullptr);
+    }
+    ++it;
+    {
+        auto v = (bool*)it;
+        BOOST_REQUIRE_NE(v, nullptr);
+        BOOST_CHECK_EQUAL(*v, std::get<3>(t));
+    }
+    it++;
+    {
+        auto v = (int*)it; // unsigned
+        BOOST_REQUIRE_EQUAL(v, nullptr);
+    }
+
+    BOOST_CHECK(--end(t) == it--);
+}
+
+BOOST_AUTO_TEST_CASE(test_for)
+{
+    using namespace QoUtils;
+    std::tuple t = {2.3, 4.3, -4, 4, false, -4.0, 3, 7};
+
+    double d_sum = 0.0;
+    double i_sum = 0;
+
+    for(auto it = begin(t); it != end(t); ++it)
+    {
+        if (auto v = (double*)it)
+        {
+            d_sum += *v;
+        }
+        else if(auto v = (int*)it)
+        {
+            i_sum += *v;
+        }
+    }
+
+    BOOST_CHECK_CLOSE_FRACTION(d_sum, 2.3 + 4.3 -4.0, 0.01);
+    BOOST_CHECK_EQUAL(i_sum, -4 + 4 + 3 + 7);
 }
 
 BOOST_AUTO_TEST_SUITE_END();
--- tuple-iterator.hpp
+++ tuple-iterator.hpp
@@ -2,38 +2,116 @@
 #define QoUtils_TUPLE_ITERATOR_HPP
 
 #include <tuple>
+#include <variant>
 
 namespace QoUtils
 {
 
-template<std::size_t I, class ...A>
+template<std::size_t I, class X, class ...XS>
+struct get_type_by
+{
+    using type = typename get_type_by<I-1, XS...>::type;
+};
+
+template<class X, class ...XS>
+struct get_type_by<0, X, XS...>
+{
+    using type = X;
+};
+
+
+template<std::size_t I, class ...XS>
+using get_type_by_t = typename get_type_by<I, XS...>::type;
+
+template<class ...T>
 struct TupleIterator
 {
-    std::tuple<A...>& m_t;
+    using iterator_category = std::random_access_iterator_tag;
+    using value_type = std::variant<T...>;
+    using difference_type = std::size_t;
 
-    constexpr
-    TupleIterator<I+1, A...> operator++() noexcept
+    constexpr auto && operator++()    noexcept { ++m_i; return *this; }
+    constexpr auto    operator++(int) noexcept { auto t = *this; operator++(); return t; }
+
+    constexpr auto && operator--()    noexcept { --m_i; return *this; }
+    constexpr auto    operator--(int) noexcept { auto t = *this; operator--(); return t; }
+
+    constexpr auto && operator[](std::size_t i) const
     {
-        return {m_t};
+        return TupleIterator<T...>{r_t, m_i + i}.operator*();
     }
 
-    constexpr
-    auto && operator*() const noexcept
+//    constexpr std::variant<std::reference_wrapper<T>...> operator*() const
+//    {
+//        return std::get<0>(r_t);
+
+////        throw std::out_of_range{
+////               "The tuple contains no item by index: "s + std::to_string(m_i)
+////                           + "; (QoUtils_MAX_COUNT_TUPLE_ITEMS = "
+////                + std::to_string(QoUtils_MAX_COUNT_TUPLE_ITEMS) + ")"};
+
+//    }
+
+    constexpr bool operator ==(const TupleIterator<T...>& t) const noexcept { return (m_i == t.m_i) && (&r_t == &t.r_t); }
+    constexpr bool operator !=(const TupleIterator<T...>& t) const noexcept { return !operator==(t); }
+
+//    template<class R, class ...RS> constexpr
+//    bool get(R *result, RS* ...) const noexcept
+//    {
+//        if constexpr (sizeof... (RS) != 0)
+//        {
+//            if (result = get_impl<R, 0>())
+//                return result;
+//            else
+//                return get<RS...>();
+//        }
+
+//        return false;
+//    }
+
+    template<class R>
+    operator R*() const noexcept
     {
-        return std::get<I>(m_t);
+        return get_impl<R, 0>();
     }
+
+private:
+    template<class R, std::size_t I = 0> constexpr
+    R* get_impl() const noexcept
+    {
+        if constexpr (I < sizeof... (T))
+        {
+            if constexpr (std::is_same_v<R, get_type_by_t<I, T...> >)
+                if(m_i == I)
+                    return &std::get<I>(r_t);
+
+            return get_impl<R, I+1>();
+        }
+
+        return nullptr;
+    }
+
+    TupleIterator(std::tuple<T...>& a, std::size_t i): r_t{a}, m_i{i} {}
+
+    std::tuple<T...>& r_t;
+    std::size_t m_i;
+
+    template<class ...B> friend constexpr auto begin(std::tuple<B...>&) noexcept;
+    template<class ...B> friend constexpr auto end  (std::tuple<B...>&) noexcept;
 };
 
 template<class ...A> constexpr
 auto begin(std::tuple<A...>& t) noexcept
 {
-    return TupleIterator<0, A...>{t};
+    return TupleIterator<A...>{t, 0};
 }
 
 template<class ...A> constexpr
 auto end(std::tuple<A...>& t) noexcept
 {
-    return TupleIterator<sizeof... (A), A...>{t};
+    constexpr auto size = sizeof...(A);
+
+    return TupleIterator<A...>{t, size};
 }
 
 }
